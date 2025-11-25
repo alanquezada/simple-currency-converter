fbb78c8baac0bc8d78a41f990b617af9d4215483
Alan Quezada <your_github_alanq@usf.edu>
2025-11-24 23:33:14 -0500

    Task 3: Add calculation based on user input

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-# Task 1: Welcome message
-print("🇲🇽 Welcome to the Simple USD to MXN Converter!")
+# app.py
+# Define the exchange rate for the calculation
+MXN_RATE = 17.15
+
+print("Welcome to the Simple USD to MXN Converter!")
+
 # Task 2: Ask for input
 usd_input = input("Enter the amount in USD ($) you wish to convert: ")
+
+# Task 3: Convert input & perform calculation
+usd_amount = float(usd_input)
+mxn_amount = usd_amount * MXN_RATE
